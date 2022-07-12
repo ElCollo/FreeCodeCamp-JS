a023f93afac8ea172f18c980221d7d9b74b8b4f5
ElCollo <isanbaev_ildar@mail.ru>
2022-07-12 16:56:28 +0700

    Update 45.js

--- 45.js
+++ 45.js
@@ -1,8 +1,3 @@
-// Example
-var ourArray = [1, 2, 3];
-var removedFromOurArray = ourArray.pop();
-// removedFromOurArray now equals 3, and ourArray now equals [1,2]
-
 // Setup
 var myArray = [["John", 23], ["cat", 2]];
 
