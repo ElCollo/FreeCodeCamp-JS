79472a584289ba4b194c9fe4fd939e5279bb1fad
ElCollo <isanbaev_ildar@mail.ru>
2022-07-12 16:56:40 +0700

    Update 44.js

--- 44.js
+++ 44.js
@@ -1,8 +1,3 @@
-// Example
-var ourArray = ["Stimpson", "J", "cat"];
-ourArray.push(["happy", "joy"]);
-// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
-
 // Setup
 var myArray = [["John", 23], ["cat", 2]];
 
